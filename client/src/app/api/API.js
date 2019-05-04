b5975a8016d121415345fce905d2fc46d0080688
August Johnson <gusjohnson98@gmail.com>
2019-05-04 00:51:42 -0500

    env

--- client/src/app/api/API.js
+++ client/src/app/api/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const MY_KEY = process.env.API_KEY;
+// const API_KEY = process.env.API_KEY;
 
 export default {
     getBooks: function (query) {
-        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&download=epub&key=" + MY_KEY);
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&download=epub&key=" + process.env.API_KEY);
     },
     getSavedBooks: function () {
         return axios.get("/api/books/saved");
